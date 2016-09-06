dc10ac96feaddceba976e0ddb83c9d37d76fc694
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-09-06 17:19:35 +0800

    add hg suggest rebase function
    give suggestion for “hg rebase” base on tip and divergent commits

---
+++ hg_suggest_rebase.fish
@@ -0,0 +1,7 @@
+function hg_suggest_rebase
+    hg pull
+    set tip (hg tip | grep changeset | cut -d":" -f3)
+    set my (hg outgoing | grep changeset | head -n 1 | cut -d":" -f3)
+    hg log -l 50 -G | grep --color -e $tip -e $my -A5
+    echo -e "\e[32m -s \e[1m $my \e[0m\e[32m -d \e[1m $tip \e[0m"
+end
