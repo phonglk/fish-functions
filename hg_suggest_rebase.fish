e452fb43496605ce9603d7fc8db7016fb5cda0e4
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-09-06 17:30:57 +0800

    copy param to global variable

--- hg_suggest_rebase.fish
+++ hg_suggest_rebase.fish
@@ -4,4 +4,5 @@ function hg_suggest_rebase
     set my (hg outgoing | grep changeset | head -n 1 | cut -d":" -f3)
     hg log -l 50 -G | grep --color -e $tip -e $my -A5
     echo -e "\e[32m -s \e[1m $my \e[0m\e[32m -d \e[1m $tip \e[0m"
+    set -g -x REBASE "-s $my -d $tip"
 end
